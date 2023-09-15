927b6c4ea054a802aecabb15733b0bc7408d15d6
Nazerke Kalidolda <nkalidolda@gmail.com>
2023-09-15 04:19:25 +0200

    final

--- floating-item/SecondView.swift
+++ floating-item/SecondView.swift
@@ -5,4 +5,122 @@
 //  Created by Nazerke Kalidolda on 15.09.2023.
 //
 
-import Foundation
+import UIKit
+
+final class MyTableView: UITableView {}
+
+extension MyTableView: UIGestureRecognizerDelegate {
+    func gestureRecognizer(_ gestureRecognizer: UIGestureRecognizer, shouldRecognizeSimultaneouslyWith otherGestureRecognizer: UIGestureRecognizer) -> Bool {
+        return true
+    }
+}
+
+protocol MyViewDelegate: AnyObject {
+    func dismissView()
+    func expandView()
+}
+
+final class SecondView: UIView {
+    
+    weak var delegate: MyViewDelegate?
+    
+    private lazy var tableView: MyTableView = {
+        let view = MyTableView()
+        view.register(TableViewCell.self, forCellReuseIdentifier: "reuseId")
+        //    view.translatesAutoresizingMaskIntoConstraints = false
+        view.backgroundColor = .blue
+        view.delegate = self
+        view.showsVerticalScrollIndicator = false
+        view.bounces = false
+        view.dataSource = self
+        return view
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        backgroundColor = .white
+        setupViews()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setupViews() {
+        addSubview(tableView)
+        
+        tableView.addGestureRecognizer(UIPanGestureRecognizer(target: self, action: #selector(handler)))
+        tableView.isScrollEnabled = false
+    }
+    
+    override func layoutSubviews() {
+        tableView.frame = bounds
+    }
+    
+    var startPos: CGFloat = 0.0;
+    var offset: CGFloat = 0.0;
+    let maxOffset: CGFloat = 100.0;
+    
+    @objc private func handler(gesture: UIPanGestureRecognizer){
+        //    print(gesture.location(in: self))
+        
+        switch gesture.state {
+        case .began:
+            startPos = gesture.location(in: self.superview).y
+        case .changed:
+            offset = gesture.location(in: self.superview).y - startPos
+            
+            let norm = (1.0 - min(1.0, offset / maxOffset)) / 2.0
+            //      print(norm)
+            
+            if tableView.contentOffset.y <= 0.0 {
+                let transform = CGAffineTransformConcat(
+                    CGAffineTransform(translationX: 0, y: offset),
+                    CGAffineTransform(scaleX: 0.5 + norm, y: 0.5 + norm)
+                )
+                self.transform = transform
+            }
+        case .ended:
+            UIView.animate(withDuration: 0.5, delay: 0.0, usingSpringWithDamping: 0.8, initialSpringVelocity: 0.7, animations: {
+                self.transform = CGAffineTransform(translationX: 0, y: 0)
+            })
+            
+            if offset > 100 && tableView.contentOffset.y <= 0{
+                // dismiss
+                delegate?.dismissView()
+                tableView.isScrollEnabled = false
+            }
+            if offset < -80 {
+                delegate?.expandView()
+                tableView.isScrollEnabled = true
+            }
+        default:
+            break
+        }
+        print(offset)
+        print(tableView.contentOffset.y)
+    }
+    
+}
+
+extension SecondView: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 50
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 50
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseId", for: indexPath) as! TableViewCell
+        cell.text = "Audio \(indexPath.row)"
+        return cell
+    }
+    
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        //    print("scroll")
+        //    print(scrollView.contentOffset)
+    }
+}
--- floating-item/ViewController.swift
+++ floating-item/ViewController.swift
@@ -8,12 +8,159 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    private var isExpanded = false;
+    
+    private lazy var titleLabel: UILabel = {
+        let label = UILabel()
+        label.frame = CGRect(x: 30, y: 80, width: view.bounds.width - 40, height: 44)
+        label.text = "Musée du Louvre"
+        label.font = UIFont.boldSystemFont(ofSize: 35)
+        label.textAlignment = .center
+        label.textColor = UIColor.blue
+        label.layer.cornerRadius = 20
+        label.layer.masksToBounds = true
+        label.isHidden = false
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    private lazy var textLabel: UILabel = {
+        let label = UILabel()
+        label.frame = CGRect(x: 30, y: 130, width: view.bounds.width - 50, height: 300)
+        label.text = "Le musée du Louvre est un musée situé dans le 1er arrondissement de Paris, en France. Une préfiguration en est imaginée en 1775-1776 par le comte d'Angiviller, directeur général des Bâtiments du roi, comme lieu de présentation des chefs-d'œuvre de la collection de la Couronne. Ce musée n'a été inauguré qu'en 1793 sous l'appellation de Muséum central des arts de la République dans le palais du Louvre, ancienne résidence royale située au centre de Paris, et il est aujourd'hui l'un des trois plus grands musées d'art du monde aux côtés du Musée de l'Ermitage et du Musée national de Chine. Sa surface d'exposition est de 72 735 m29."
+        label.font = UIFont.systemFont(ofSize: 16)
+        label.textAlignment = .left
+        label.numberOfLines = 0
+        label.textColor = UIColor.darkGray
+        label.layer.cornerRadius = 20
+        label.layer.masksToBounds = true
+        label.isHidden = false
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    private lazy var button: UIButton = {
+        let button = UIButton(frame: CGRect(x: 50, y: 200, width: 50, height: 50))
+        button.backgroundColor = .blue.withAlphaComponent(0.7)
+        button.layer.cornerRadius = 25.0
+        button.addGestureRecognizer(UIPanGestureRecognizer(target: self, action: #selector(handler)))
+        button.addTarget(self, action: #selector(didTapButton), for: .touchUpInside)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
+    private lazy var secondView: SecondView = {
+        let view = SecondView()
+        view.layer.cornerRadius = 20
+        view.layer.masksToBounds = true
+        view.isHidden = false
+        view.delegate = self
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        view.backgroundColor = .white
+        
+        setupViews()
+        
+        self.view.bringSubviewToFront(button)
     }
+    
+    private func setupViews() {
+        [titleLabel, textLabel, button, secondView].forEach {
+            view.addSubview($0)
+        }
+    }
+    
+    @objc private func handler(gesture: UIPanGestureRecognizer){
+        print(gesture.location(in: self.view))
+        switch gesture.state {
+        case .changed:
+            button.center = gesture.location(in: view)
+        case .ended:
+            animateToEdge()
+        default:
+            break
+        }
+        
+    }
+    
+    private func animateToEdge() {
+        var newX: CGFloat = 0.0
+        if button.frame.midX < view.frame.midX {
+            newX = 16
+        } else {
+            newX = view.frame.width - 66
+        }
+        
+        UIView.animate(withDuration: 0.5, delay: 0.0, usingSpringWithDamping: 0.8, initialSpringVelocity: 0.7, animations: {
+            self.button.frame = CGRect(x: newX,
+                                       y: self.button.frame.minY,
+                                       width: self.button.frame.width,
+                                       height: self.button.frame.height)
+        })
+        
+    }
+    
+    @objc private func didTapButton() {
+        print("tap button")
+        isExpanded.toggle()
+        animateExpand(expand: isExpanded)
+        
+    }
+    
+    private func animateExpand(expand: Bool) {
+        if expand {
+            // Show animation
+            secondView.frame = button.frame
+            secondView.layer.cornerRadius = button.frame.width / 2
+            secondView.isHidden = false
+            secondView.alpha = 1.0
+            
+            UIView.animate(withDuration: 0.5, delay: 0.0, usingSpringWithDamping: 0.8, initialSpringVelocity: 0.7, animations: {
+                self.secondView.frame = CGRect(x: self.view.center.x-100,
+                                               y: self.view.center.y-150,
+                                               width: 200,
+                                               height: 300)
+                self.secondView.layer.cornerRadius = 10
+                
+            }) { _ in
+                print("done")
+            }
+        } else {
+            // Hide animation
+            UIView.animate(withDuration: 0.1) {
+                self.secondView.alpha = 0.5
+            }
+            UIView.animate(withDuration: 0.5, delay: 0.0, usingSpringWithDamping: 0.8, initialSpringVelocity: 0.7, animations: {
+                self.secondView.frame = self.button.frame
+                self.secondView.layer.cornerRadius = self.button.frame.width / 2
+            }) { _ in
+                self.secondView.isHidden = true
+            }
+        }
+    }
+    
+    private func expand() {
+        UIView.animate(withDuration: 0.5, delay: 0.0, usingSpringWithDamping: 0.8, initialSpringVelocity: 0.7, animations: {
+            self.secondView.frame = self.view.frame
+        })
+    }
+    
+}
 
-
+extension ViewController: MyViewDelegate {
+    func dismissView() {
+        isExpanded = false
+        animateExpand(expand: isExpanded)
+    }
+    
+    func expandView() {
+        expand()
+    }
 }
 
