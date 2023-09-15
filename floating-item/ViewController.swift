56f5b559d5d997550b314294e36baae746069de8
Nazerke Kalidolda <nkalidolda@gmail.com>
2023-09-15 04:04:33 +0200

    Initial Commit

---
+++ floating-item/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  floating-item
+//
+//  Created by Nazerke Kalidolda on 15.09.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
